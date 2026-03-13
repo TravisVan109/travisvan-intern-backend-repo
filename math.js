308b91d9e16d7cff706a009de5afe92581262fb1
Văn Công Thành <vancongthanh109@gmail.com>
2026-03-13 11:24:58 +1100

    Add multiply() function

--- math.js
+++ math.js
@@ -1,5 +1,13 @@
+//function to add two numbers
 function add(a, b) {
     return a + b;
 }
 
 console.log(add(2, 3)); // Expected output: 5
+
+//function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 3)); // Expected output: 6
