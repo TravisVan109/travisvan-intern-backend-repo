df70841bce7a6372901c7aa0cb954039810f5ba3
Văn Công Thành <vancongthanh109@gmail.com>
2026-04-06 15:02:03 +1000

    feat: added reflection and practices about Using Interceptors & Middleware in NestJS

--- my-nest-app/src/app.module.ts
+++ my-nest-app/src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { ItemsModule } from './items/items.module';
+import { LoggerMiddleware } from './logging/logger.middleware';
 
 @Module({
   imports: [CatsModule, UsersModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*'); // or specific route like 'items'
+  }
+}
--- my-nest-app/src/main.ts
+++ my-nest-app/src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { LoggingInterceptor } from './logging/logging.interceptor';
+import { ClassSerializerInterceptor } from '@nestjs/common/serializer/class-serializer.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.useGlobalPipes(new ValidationPipe());// Enable validation globally
+  app.useGlobalPipes(new ValidationPipe());// Enable validation globally
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
