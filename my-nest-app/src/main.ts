ee32413bff69e561036446f3efe5446a3c242c1a
Văn Công Thành <vancongthanh109@gmail.com>
2026-04-11 10:28:00 +1000

    feat: added reflection and practices about 8.3 Logging & Error Handling in NestJS

--- my-nest-app/src/main.ts
+++ my-nest-app/src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { LoggingInterceptor } from './logging/logging.interceptor';
 import { ClassSerializerInterceptor } from '@nestjs/common/serializer/class-serializer.interceptor';
+import { Logger } from 'nestjs-pino';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());// Enable validation globally
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useLogger(app.get(Logger));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
